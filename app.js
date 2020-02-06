ccc894b7a9d065c5b9069ccdb02bbeb6c613b225
Haranselvaraj <hariharans970@gmail.com>
2020-02-06 23:02:01 +0530

    login form completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
-const morgan = require ('morgan');
 const mongoose = require ('mongoose');
+const session = require('express-session');
 
 var app = express();
 
@@ -9,6 +9,12 @@ app.set('view engine','ejs');
 app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+   secret: 'sdfghjkl',
+   resave: true,
+   saveUninitialized: true,
+}));
 
 
 mongoose.connect('mongodb://localhost', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
@@ -27,19 +33,60 @@ var cake = new mongoose.Schema({
    type:String
 });
 
+var logdb = new mongoose.Schema({
+  name:String,
+  pwd: String
+});
+
+var loguser = mongoose.model('loguser',logdb)
+
+
 var Cakes = mongoose.model('Cakes',cake)
 
-app.get('/' ,(req,res) => {
-   Cakes.find({}, (err,data) =>{
-    if(err){
-       throw err
-    }else{
-       res.render('index',{items:data})
-       console.log({items:data});
+// app.get('/' ,(req,res) => {
+//    Cakes.find({}, (err,data) =>{
+//     if(err){
+//        throw err
+//     }else{
+//        res.render('index',{items:data})
+//        console.log(data);
        
-    }
-   })
+//     }
+//    })
+// });
+
+app.get('/login' , (req,res) =>{
+   res.render('login');
 });
+
+app.post('/auth' , (req,res) =>{
+ 
+   var user=req.body.username;
+   var pwd=req.body.password;
+   var a='hari';
+   var b='haran';
+   if(a == user && b == pwd){
+      req.session.loggedin = true;
+      res.redirect('/')
+   }
+})
+
+app.get('/', (req,res) =>{
+   if(req.session.loggedin){
+      Cakes.find({}, (err,data) =>{
+         if(err){
+            throw err
+         }else{
+            res.render('index',{items:data})
+            console.log(data);
+            
+         }
+        });
+   }
+   else{
+      res.redirect('/login')
+   }
+})
 
 app.listen(5000, ()=>{console.log('Server is running!');
 });
