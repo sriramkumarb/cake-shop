a04f12127a8703824129c8bfa27883ba6c0b7622
Haranselvaraj <hariharans970@gmail.com>
2020-02-07 14:43:10 +0530

    signup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const mongoose = require ('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const session = require('express-session');
 
 var app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,30 +18,32 @@ app.use(session({
 
 
 mongoose.connect('mongodb://localhost', { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    if (err) {
-        throw err
-    } else {
-        console.log('MongoDb Connected');
-        
-    }
+   if (err) {
+      throw err
+   } else {
+      console.log('MongoDb Connected');
+
+   }
 });
 
 var cake = new mongoose.Schema({
-   item :String,
-   price : Number,
-   img :String,
-   type:String
+   item: String,
+   price: Number,
+   img: String,
+   type: String
 });
 
 var logdb = new mongoose.Schema({
-  name:String,
-  pwd: String
+   name: String,
+   mail: String,
+   pwd: String,
+
 });
 
-var loguser = mongoose.model('loguser',logdb)
+var loguser = mongoose.model('loguser', logdb)
 
 
-var Cakes = mongoose.model('Cakes',cake)
+var Cakes = mongoose.model('Cakes', cake)
 
 // app.get('/' ,(req,res) => {
 //    Cakes.find({}, (err,data) =>{
@@ -50,43 +52,62 @@ var Cakes = mongoose.model('Cakes',cake)
 //     }else{
 //        res.render('index',{items:data})
 //        console.log(data);
-       
+
 //     }
 //    })
 // });
 
-app.get('/login' , (req,res) =>{
+app.get('/login', (req, res) => {
    res.render('login');
 });
 
-app.post('/auth' , (req,res) =>{
- 
-   var user=req.body.username;
-   var pwd=req.body.password;
-   var a='hari';
-   var b='haran';
-   if(a == user && b == pwd){
+app.post('/auth', (req, res) => {
+
+   var user = req.body.username;
+   var pwd = req.body.password;
+
+   if (loguser.find({ name: user, pwd: pwd })) {
       req.session.loggedin = true;
+      req.session.username = user;
       res.redirect('/')
    }
 })
 
-app.get('/', (req,res) =>{
-   if(req.session.loggedin){
-      Cakes.find({}, (err,data) =>{
-         if(err){
-            throw err
-         }else{
-            res.render('index',{items:data})
-            console.log(data);
-            
+app.get('/', (req, res) => {
+   Cakes.find({}, (err, data) => {
+      if (err) {
+         throw err
+      } else {
+         if (req.session.loggedin) {
+            res.render('index', { items: data, name: req.session.username })
          }
-        });
-   }
-   else{
-      res.redirect('/login')
-   }
+         else {
+            res.render('index', { items: data, name: null })
+         }
+      }
+   });
 })
 
-app.listen(5000, ()=>{console.log('Server is running!');
+app.get('/signup', (req, res) => {
+   res.render('Register')
+})
+
+app.post('/signup', (req, res) => {
+   var signup = loguser(req.body).save((err, data) => {
+      if (err) {
+         throw err
+      } else {
+         res.redirect('login')
+
+      }
+   })
+
+})
+
+app.post('/login', (req, res) => {
+   res.render('login')
+})
+
+app.listen(5000, () => {
+   console.log('Server is running!');
 });
